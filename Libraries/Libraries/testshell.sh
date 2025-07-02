34a98e7e2e95a8113b8dcc453704b9656288437f
Retraxi <raphael_tanai@dlsu.edu.ph>
2025-07-02 09:30:34 +0800

    more benchmark updates

--- Libraries/Libraries/testshell.sh
+++ Libraries/Libraries/testshell.sh
@@ -1,14 +1,11 @@
-# Benchmark
-gcc benchmark.c -O3 -g -o benchmark -lm
-
-#iterate
-libraries=("biwavex" "biwfa_avx" "biwfa_og" "ksw2" "nw")
+# loop
 testcases=("10K" "20K" "30K" "40K" "50K" "60K" "70K" "80K" "90K" "100K")
+libraries=("biwavex" "biwfa_avx" "biwfa_og" "ksw2" "nw")
 
 for i in "${libraries[@]}"
 do
     for x in "${testcases[@]}"
     do
-        ./benchmark -l $i -t $x
+        echo "Library [$i] on Case [$x]..."
     done
 done
